7f47b5d3ef391f5d44cd0661d1e82428fb991bd5
FatemehKhaghanpoor <khaghanpoor@gmail.com>
2024-11-28 03:39:42 -0800

    add input and button todolist

--- src/component/main/index.jsx
+++ src/component/main/index.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useState } from "react";
 
 const Main = () => {
+  const [newItem , setNewItem]= useState();
+  const addItem=()=>{
+    console.log(newItem);
+    
+  }
   return (
    <div>
-    
+    <div className="justify-self-center " > 
+      <input type="text"
+      placeholder="add an item"
+      value={newItem}
+      onChange={e =>setNewItem(e.target.value)}
+       />
+       <button onClick={()=>{addItem()}}>add</button>
+    </div>
+    <div className="justify-self-center ">
+      <ul>
+        <li>take out</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+      </ul>
+    </div>
    </div>
   );
 };
